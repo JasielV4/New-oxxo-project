4c3c110543828878b4b99bc1b2a7a01b87eedcf7
JasielV4 <bioplayer1234@gmail.com>
2024-10-05 19:55:40 -0600

    Roles guard implemented

--- src/auth/constants/jwt.constanst.ts
+++ src/auth/constants/jwt.constanst.ts
@@ -1,2 +1,2 @@
-export const JWT_KEY = 'GJRASJD!#@$!@#R#FGFDS!@#R$#!#@!#';
-export const EXPIRES_IN = '30s'; 
+export const JWT_KEY = 'CAJT!#@!@#/3#JSVG!@#R$/&#!#';
+export const EXPIRES_IN = '120s'; 
