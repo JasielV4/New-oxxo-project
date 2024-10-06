3ef732b2c2412301368d3a7cfd53bbbd88899187
JasielV4 <bioplayer1234@gmail.com>
2024-10-05 21:31:21 -0600

    Auth for routes

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,44 +1,55 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) { }
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @Auth(ROLES.MANAGER, ROLES.EMPLOYEE)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadPhoto(@UploadedFile() file: Express.Multer.File) {
-    return "OK"
+    return "OK";
   }
 
-
+  @Auth(ROLES.MANAGER)
   @Get()
   findAll() {
     return this.employeesService.findAll();
   }
 
-  @Get(':id')
+  @Auth(ROLES.MANAGER)
+  @Get('/:id')
   findOne(
     @Param('id', new ParseUUIDPipe({ version: '4' }))
-    id: string) {
+    id: string
+  ) {
     return this.employeesService.findOne(id);
   }
 
-  @Patch(':id')
+  @Auth(ROLES.EMPLOYEE)
+  @Patch('/:id')
   update(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
     return this.employeesService.update(id, updateEmployeeDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+  @Auth(ROLES.MANAGER)
+  @Delete('/:id')
+  remove(
+    @Param('id', new ParseUUIDPipe({ version: '4' }))
+    id: string
+  ) {
     return this.employeesService.remove(id);
   }
 }
